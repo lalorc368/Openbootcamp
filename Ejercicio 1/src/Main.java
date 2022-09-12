7ebdd0a9f82a4359b927d718a3ad6f3bd96a8d63
lalorc368 <lalo.aries7@gmail.com>
2022-09-11 19:06:09 -0500

    Primer Ejercicio

---
+++ Ejercicio 1/src/Main.java
@@ -0,0 +1,16 @@
+// Ejercicio 1
+
+public class Main {
+    public static void main(String[] args) {
+
+        int resultado;
+        resultado = suma( 12,24,26);
+
+        System.out.println(resultado);
+    }
+
+    public static int suma(int a, int b, int c){
+        return a + b + c;
+    }
+
+}
