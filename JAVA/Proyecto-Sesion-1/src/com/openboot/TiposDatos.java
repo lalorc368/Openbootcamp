d5337077c805bcc541fa777a4bbb93bf46a4e781
lalorc368 <lalo.aries7@gmail.com>
2022-10-05 00:10:15 -0500

    Entrega Ejercicio Tema 1

---
+++ JAVA/Proyecto-Sesion-1/src/com/openboot/TiposDatos.java
@@ -0,0 +1,51 @@
+package com.openboot;
+
+public class TiposDatos {
+
+    public static void main(String[] args) {
+
+        // Tipo de datos numericos
+
+        System.out.println("Numericas");
+
+        byte variable1 = 1;
+        System.out.println(variable1);
+
+        short variable2 = 2;
+        System.out.println(variable2);
+
+        int variable3 = 3;
+        System.out.println(variable3);
+
+        long variable4 = 4;
+        System.out.println(variable4);
+
+        float variable5 = 5.5f;
+        System.out.println(variable5);
+
+        double variable6 = 6.66d;
+        System.out.println(variable6);
+
+        // Tipo de datos booleanos
+
+        System.out.println("Booleanos");
+
+        boolean variable7 = true;
+        System.out.println(variable7);
+
+        boolean variable8 = false;
+        System.out.println(variable8);
+
+        // Tipo de datos Texto
+
+        System.out.println("Tipo de datos Texto");
+
+        String variable9 = "Prueba de Texto";
+        System.out.println(variable9);
+
+        char variable10= 'A';
+        System.out.println(variable10);
+
+
+    }
+}
