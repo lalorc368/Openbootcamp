6b3c5153ed1e48af431c2c1a39419de6a2ebd307
lalorc368 <lalo.aries7@gmail.com>
2022-10-06 23:43:57 -0500

    Ejercio Tema 2

---
+++ JAVA/Proyecto-Sesion1-Tema2/src/com/open/PrecioIva.java
@@ -0,0 +1,15 @@
+package com.open;
+
+public class PrecioIva {
+
+    public static void main(String[] args) {
+
+        int precio = 120;
+        double iva = 19.2;
+        double precioIva = suma(precio + iva);
+        System.out.println(precioIva);
+    }
+    static double suma(double numero1){
+        return numero1;
+    }
+}
