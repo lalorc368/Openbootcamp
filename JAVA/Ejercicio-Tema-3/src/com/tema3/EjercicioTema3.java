d71b9eaad6b7bc014ead41b07b145164cab7f229
lalorc368 <lalo.aries7@gmail.com>
2022-10-12 00:26:27 -0500

    Ejercicio Sesion 3

---
+++ JAVA/Ejercicio-Tema-3/src/com/tema3/EjercicioTema3.java
@@ -0,0 +1,14 @@
+package com.tema3;
+
+public class EjercicioTema3 {
+
+    public static void main(String[] args) {
+
+        String[] nombres = {"Eduardo", "Fernanda", "Daniela", "Mauricio", "Andre"};
+
+        for (String nombre : nombres) {
+            System.out.println(nombre);
+        }
+
+    }
+}
